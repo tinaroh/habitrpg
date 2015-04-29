bfd5c48c892e1284f828d9f3c56f7588b6008b4f
Blade Barringer <blade@crookedneighbor.com>
2015-04-28 21:26:46 -0500

    Refactored tests for save

--- test/spec/challengeCtrlSpec.js
+++ test/spec/challengeCtrlSpec.js
@@ -1,14 +1,14 @@
 'use strict';
 
 describe('Challenges Controller', function() {
-  var scope, ctrl, user, challenges, $rootScope;
+  var scope, ctrl, user, Challenges, Notification, User, $rootScope;
 
   beforeEach(function() {
     module(function($provide) {
       $provide.value('User', {});
     });
 
-    inject(function($rootScope, $controller, Challenges){
+    inject(function($rootScope, $controller, _Challenges_, _Notification_, _User_){
       user = specHelper.newUser();
       user._id = "unique-user-id";
 
@@ -19,66 +19,117 @@ describe('Challenges Controller', function() {
 
       ctrl = $controller('ChallengesCtrl', {$scope: scope, User: {user: user}});
 
-      challenges = Challenges;
+      Challenges = _Challenges_;
+      Notification = _Notification_;
+      User = _User_;
     });
   });
 
   describe("save challenge", function() {
     var alert;
 
-    beforeEach(function(){
+    before(function(){
       alert = sinon.stub(window, "alert");
     });
 
-    afterEach(function(){
+    after(function(){
       window.alert.restore();
     });
 
     it("opens an alert box if challenge.group is not specified", function() {
-      // @TODO: change these to new real challenges using challenges constructor
-      var chlg = {
-        name: "Challenge without a group",
-        leader: user._id
-      };
+      var challenge = new Challenges.Challenge({
+        name: 'Challenge without a group',
+        description: '',
+        habits: [],
+        dailys: [],
+        todos: [],
+        rewards: [],
+        leader: user._id,
+        timestamp: +(new Date),
+        members: [],
+        official: false
+      });
 
-      scope.save(chlg);
-      expect(alert.callCount).to.to.eql(1);
+      scope.save(challenge);
+
       alert.should.have.been.calledWith(window.env.t('selectGroup'));
     });
 
     it("opens an alert box if isNew and user does not have enough gems", function() {
-      var chlg = {
-        name: "Challenge without enough gems",
+      var challenge = new Challenges.Challenge({
+        name: 'Challenge without enough gems',
+        description: '',
+        habits: [],
+        dailys: [],
+        todos: [],
+        rewards: [],
         leader: user._id,
-        group: "a-group-id"
-      };
+        group: "a-group-id",
+        timestamp: +(new Date),
+        members: [],
+        official: false
+      });
 
       scope.enoughGems = false;
-      scope.save(chlg);
+      scope.save(challenge);
 
-      expect(alert.callCount).to.to.eql(1);
       alert.should.have.been.calledWith(window.env.t('challengeNotEnoughGems'));
     });
 
     it("saves the challenge if user does not have enough gems, but the challenge is not new", function() {
 
-      var chlg = {
-        _id: "challenge-id",
-        name: "Challenge that is not new",
+      var challenge = new Challenges.Challenge({
+        _id: 'challeng-id',
+        name: 'Challenge without enough gems',
+        description: '',
+        habits: [],
+        dailys: [],
+        todos: [],
+        rewards: [],
         leader: user._id,
         group: "a-group-id",
+        timestamp: +(new Date),
+        members: [],
+        official: false,
+        // Mock $save
         $save: function(cb) {
           cb();
         }
-      };
+      });
 
       scope.enoughGems = false;
-      scope.save(chlg);
+      scope.save(challenge);
 
-      expect(chlg._locked).to.be.ok;
+      expect(challenge._locked).to.be.ok;
+    });
 
-      // @TODO stub getChallenges and make sure it gets called
-      // Made need to put getChallenges on $scope to do that
+    it("saves the challenge if user has enough gems and challenge is new", function() {
+      var saveWasCalled = false;
+
+      var challenge = new Challenges.Challenge({
+        name: 'Challenge without enough gems',
+        description: '',
+        habits: [],
+        dailys: [],
+        todos: [],
+        rewards: [],
+        leader: user._id,
+        group: "a-group-id",
+        timestamp: +(new Date),
+        members: [],
+        official: false,
+        // Mock $save
+        $save: function() {
+          saveWasCalled = true;
+          return;
+        }
+      });
+
+      scope.enoughGems = true;
+      scope.save(challenge);
+
+      // Not the best test, but :shrug:
+      expect(saveWasCalled).to.be.ok;
     });
   });
 });
